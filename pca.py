8e3e221c7298643ec9c5f5110b20904a2fc73360
StephanEnseleit <stephan.enseleit@gmail.com>
2022-04-04 13:53:00 +0100

    first pca version

---
+++ pca.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from mpl_toolkits.mplot3d import Axes3D
+
+df = pd.read_csv('water_potability_cleaned_mean.csv')
+print(df.head())
+# Scale data before applying PCA
+scaling=StandardScaler()
+
+# Use fit and transform method
+scaling.fit(df)
+Scaled_data=scaling.transform(df)
+
+# Set the n_components=3
+principal=PCA(n_components=3)
+principal.fit(Scaled_data)
+x=principal.transform(Scaled_data)
+principalDf = pd.DataFrame(data = x, columns = ['pc1', 'pc2', "pc3"])
+
+# import relevant libraries for 3d graph
+from mpl_toolkits.mplot3d import Axes3D
+fig = plt.figure(figsize=(10,10))
+ 
+# choose projection 3d for creating a 3d graph
+axis = fig.add_subplot(111, projection='3d')
+ 
+# x[:,0]is pc1,x[:,1] is pc2 while x[:,2] is pc3
+axis.scatter(x[:,0],x[:,1],x[:,2], c=df['Potability'],cmap='plasma')
+axis.set_xlabel("PC1", fontsize=10)
+axis.set_ylabel("PC2", fontsize=10)
+axis.set_zlabel("PC3", fontsize=10)
+plt.show()
